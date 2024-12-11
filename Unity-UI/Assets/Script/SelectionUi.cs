71d8e59ca1d513418a48d4000ec415e9623f05aa
yukioroshi <148077899+yukioroshi@users.noreply.github.com>
2024-12-11 18:58:46 +0100

    test

--- Unity-UI/Assets/Script/SelectionUi.cs
+++ Unity-UI/Assets/Script/SelectionUi.cs
@@ -1,19 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class SelectionUi : MonoBehaviour
 {
+
+    public Button LeftHand, RightHand, Object;
+        
+
+    public GameObject ObjectInterface;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ObjectInterface.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+     public void OpenUi()
+    {
+        ObjectInterface.SetActive(true);
+    }
+
+    public void SelectLeftHand()
+    {
+
+    }
+    public void SelectRightHand()
+    {
+
     }
 }
