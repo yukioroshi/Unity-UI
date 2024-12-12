3b0f3f1a8a413a00ab237284fa2bfe8b88df8ec7
yukioroshi <148077899+yukioroshi@users.noreply.github.com>
2024-12-12 12:24:25 +0100

    t

--- Unity-UI/Assets/Script/Highlights.cs
+++ Unity-UI/Assets/Script/Highlights.cs
@@ -23,34 +23,16 @@ public class Highlights : MonoBehaviour
         mousePos = cam.ScreenToWorldPoint(mousePos);
         Debug.DrawRay(transform.position, mousePos - transform.position, Color.blue);
 
-        //if (Input.GetMouseButtonDown(0))
-        //{
-        //    Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-        //    RaycastHit hit;
-
-        //    if (Physics.Raycast(ray, out hit, 100, mask))
-        //    {
-        //        Debug.Log(hit.transform.name);
-        //        hit.transform.GetComponent<Renderer>().material.color = Color.red;
-        //    }
-        //}
-    }
-
-    private void OnMouseOver()
-    {
-        // Draw Ray
-        Vector3 mousePos = Input.mousePosition;
-        mousePos.z = 100f;
-        mousePos = cam.ScreenToWorldPoint(mousePos);
-        Debug.DrawRay(transform.position, mousePos - transform.position, Color.blue);
-
-        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit, 100, mask))
+        if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log(hit.transform.name);
-            hit.transform.GetComponent<Renderer>().material.color = Color.red;
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, mask))
+            {
+                Debug.Log(hit.transform.name);
+                hit.transform.GetComponent<Renderer>().material.color = Color.red;
+            }
         }
     }
 }
